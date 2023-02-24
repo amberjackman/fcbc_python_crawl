d52bfcab907f47caeb3bf90d1094d11133bae96a
amberjackman <awakekaw166@protonmail.ch>
2023-02-24 23:55:55 +0900

    idx_

--- crawl.py
+++ crawl.py
@@ -4,14 +4,24 @@ from bs4 import BeautifulSoup
 res = requests.get("http://www.yes24.com/24/Category/BestSeller")
 soup = BeautifulSoup(res.text, "html.parser")
 
-for i in range(40):
+for i in range(3):
   idx = str(i+1)
-  if idx == "19":
-    idx = "19_line"
-  elif idx == "20":
-      idx = "20_line"
 
   sstr = "#bestList > ol > li.num" + idx + " > p:nth-child(3) > a "
   ts = soup.select_one(sstr)
+  link = ts.attrs.get('href')
+  
+  res2 = requests.get("http://www.yes24.com" + link)
+  soup2 = BeautifulSoup(res2.text, "html.parser")
 
-  print(ts.text)
+  sale_point = soup2.select_one("#yDetailTopWrap > div.topColRgt > div.gd_infoTop > span.gd_ratingArea > span.gd_sellNum")
+  
+  #첫번째 판매지수 삭제
+  idx = sale_point.text.find('판매')
+  point = sale_point.text[idx+5:]
+
+  #두번째 판매지수 삭제
+  idx= point.find('판매')
+  point = point[:idx-1]
+
+  print(ts.text, point)
